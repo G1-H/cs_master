6bdb3b38c4a2583dfadc171634f754245ac59057
G1-H <hwangjiwon1992@gmail.com>
2025-01-12 19:13:39 +0900

    스프링 리액트 프로젝트 최초 설정

---
+++ src/main/java/com/csmaster/cs_master/CsMasterApplication.java
@@ -0,0 +1,13 @@
+package com.csmaster.cs_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CsMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CsMasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/csmaster/cs_master/CsMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.csmaster.cs_master;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CsMasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
