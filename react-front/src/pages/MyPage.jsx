4018e998b304a049a3b4f35501ce63c1ab0723f2
G1-H <hwangjiwon1992@gmail.com>
2025-01-23 22:32:17 +0900

    Design : 마이페이지

--- react-front/src/pages/MyPage.jsx
+++ react-front/src/pages/MyPage.jsx
@@ -1,7 +1,83 @@
+import { useState } from "react";
+import LeftListGroup from "../components/LeftListGroup";
+
+import { Outlet } from "react-router-dom";
+const exampleContents = [
+  { title: "내가 푼 문제 ex1", to: "/" },
+  { title: "ex2", to: "/" },
+  { title: "ex3", to: "/" },
+  { title: "ex4", to: "/" },
+  { title: "ex5", to: "/" },
+  { title: "ex6", to: "/" },
+  { title: "ex7", to: "/" },
+  { title: "ex8", to: "/" },
+  { title: "ex9", to: "/" },
+  { title: "ex10", to: "/" },
+];
+const exampleContents2 = [
+  { title: "제출한 퀴즈 ex1", to: "/" },
+  { title: "ex2", to: "/" },
+  { title: "ex3", to: "/" },
+  { title: "ex4", to: "/" },
+  { title: "ex5", to: "/" },
+  { title: "ex6", to: "/" },
+  { title: "ex7", to: "/" },
+  { title: "ex8", to: "/" },
+  { title: "ex9", to: "/" },
+  { title: "ex10", to: "/" },
+];
+const exampleContents3 = [
+  { title: "북마크 개념 ex1", to: "/" },
+  { title: "ex2", to: "/" },
+  { title: "ex3", to: "/" },
+  { title: "ex4", to: "/" },
+  { title: "ex5", to: "/" },
+  { title: "ex6", to: "/" },
+  { title: "ex7", to: "/" },
+  { title: "ex8", to: "/" },
+  { title: "ex9", to: "/" },
+  { title: "ex10", to: "/" },
+];
 const MyPage = (props) => {
+  const [contents, setContents] = useState([]);
+  const items = [
+    { title: "상세정보", to: "/mypage/info" },
+    { title: "CS 등급", to: "/mypage/grade" },
+    { title: "내가 푼 문제", to: "/mypage/submit-quiz" },
+    { title: "내가 학습한 개념", to: "/mypage/study-concept" },
+    { title: "북마크 개념", to: "/mypage/bookmark-concept" },
+  ];
+
+  const styles = {
+    listGroupItem: {
+      width: "20vw",
+    },
+  };
+  const setSelectedContent = (index) => {
+    switch (index) {
+      case 2:
+        setContents(exampleContents);
+        break;
+      case 3:
+        setContents(exampleContents2);
+        break;
+      case 4:
+        setContents(exampleContents3);
+        break;
+      default:
+        setContents([]);
+    }
+  };
   return (
-    <div>
-      <span>마이 페이지 </span>{" "}
+    <div className="main-container-row">
+      <div style={{ width: "20vw", marginTop: "30px", padding: "30px" }}>
+        <LeftListGroup
+          items={items}
+          styles={styles}
+          onSelect={setSelectedContent}
+        ></LeftListGroup>
+      </div>
+      <Outlet context={{ contents, styles }}></Outlet>
     </div>
   );
 };
