5e587dd568f108be6e054a6e6ee691c60a06030d
G1-H <hwangjiwon1992@gmail.com>
2025-01-24 23:10:07 +0900

    Design : 개념 업데이트 페이지

---
+++ react-front/src/pages/ConceptUpdate.jsx
@@ -0,0 +1,108 @@
+import { useState } from "react";
+import { InputGroup, Form, Button } from "react-bootstrap";
+import { useLocation } from "react-router-dom";
+const ConceptUpdate = () => {
+  const location = useLocation();
+  const {
+    detail: initialDetail,
+    title: initialTitle,
+    category,
+  } = location.state || {
+    detail: "내용 없음",
+    title: "제목 없음",
+  };
+
+  const [title, setTitle] = useState(initialTitle);
+  const handleTitleChange = (e) => {
+    setTitle(e.target.value);
+  };
+  const [detail, setDetail] = useState(initialDetail);
+  const handleDetailChange = (e) => {
+    setDetail(e.target.value);
+  };
+
+  return (
+    <div className="main-container">
+      <div
+        style={{
+          marginTop: "30px",
+          padding: "30px",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <div>
+          <h3>개념을 수정해 주세요.</h3>
+        </div>
+        <div style={{ width: "80%" }}>
+          <Form style={{ width: "80%" }}>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>개념 분류</InputGroup.Text>
+              <Form.Select
+                aria-label="category select"
+                value={category}
+                disabled
+              >
+                <option>개념을 선택해주세요.</option>
+                <option value="network">네트워크</option>
+                <option value="data-tree">자료구조</option>
+                <option value="os">운영체제</option>
+                <option value="database">데이터베이스</option>
+                <option value="java">자바</option>
+                <option value="js">자바스크립트</option>
+                <option value="it-commons">IT 상식</option>
+                <option value="web">웹개발</option>
+              </Form.Select>
+            </InputGroup>
+            <InputGroup className="mb-3">
+              <InputGroup.Text>개념</InputGroup.Text>
+              <Form.Control
+                placeholder="개념명"
+                aria-label="title"
+                aria-describedby="basic-addon1"
+                value={title}
+                onChange={handleTitleChange}
+              />
+            </InputGroup>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Text>상세설명</InputGroup.Text>
+              <Form.Control
+                as="textarea"
+                rows={3}
+                placeholder="상세 설명을 입력해주세요."
+                aria-label="detail"
+                aria-describedby="basic-addon2"
+                value={detail}
+                onChange={handleDetailChange}
+              />
+            </InputGroup>
+            <Form.Group controlId="formFile" className="mb-3">
+              <Form.Control type="file" />
+            </Form.Group>
+
+            <InputGroup className="mb-3">
+              <InputGroup.Text>작성자</InputGroup.Text>
+              <Form.Control
+                type="text"
+                placeholder="G1"
+                aria-label="author"
+                disabled
+                readOnly
+              />
+            </InputGroup>
+
+            <InputGroup>
+              <Button variant="primary" type="submit">
+                Submit
+              </Button>
+            </InputGroup>
+          </Form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ConceptUpdate;
