c9c105659c16e53128682dbbfabfba005965ac74
G1-H <hwangjiwon1992@gmail.com>
2025-01-24 23:10:57 +0900

    Design : 퀴즈 인덱스 페이지

--- react-front/src/pages/Quiz.jsx
+++ react-front/src/pages/Quiz.jsx
@@ -1,7 +1,36 @@
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+
 const Quiz = (props) => {
   return (
-    <div>
-      <span>퀴즈 페이지 </span>{" "}
+    <div
+      className="main-container"
+      style={{
+        alignItems: "center",
+        justifyContent: "center",
+      }}
+    >
+      <ListGroup
+        style={{
+          flexDirection: "column",
+          gap: "5px",
+
+          padding: "5px",
+          backgroundColor: "#f5d5d5",
+        }}
+      >
+        <ListGroup.Item className="quiz-list">
+          <strong className="d-block">오늘의 퀴즈 풀기 </strong>
+          <small>하루 10문제 씩 풀어보세요.</small>
+        </ListGroup.Item>
+        <ListGroup.Item className="quiz-list">
+          <strong className="d-block">카테고리 별로 풀기</strong>
+          <small>카테고리 내 모든 문제 풀어보세요.</small>
+        </ListGroup.Item>
+        <ListGroup.Item className="quiz-list">
+          <strong className="d-block">무작위 풀기</strong>
+          <small>모든 문제 무작위로 풀어보세요.</small>
+        </ListGroup.Item>
+      </ListGroup>
     </div>
   );
 };
