1358a27b320c0858447fc7cb3990bc9da4663377
G1-H <hwangjiwon1992@gmail.com>
2025-01-24 18:35:06 +0900

    Design : detailview 컴포넌트 부분 변경

--- react-front/src/components/DetailView.jsx
+++ react-front/src/components/DetailView.jsx
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import { Image } from "react-bootstrap";
 import heartFillSvg from "../assets/heart-fill.svg";
 import starFillSvg from "../assets/star-fill.svg";
+import DropdwonButton from "./DropdownButton";
 
 const DetailView = () => {
   const location = useLocation();
@@ -9,6 +10,15 @@ const DetailView = () => {
     title: "제목 없음",
     detail: "내용이 없습니다.",
   };
+  const reportDropdown = [
+    { label: "이전버전으로", href: "#/action-1" },
+    { label: "삭제 요청", href: "#/action-2" },
+  ];
+  const updateDropdown = [
+    { label: "문제 만들기", href: "#/action-1" },
+    { label: "개념 수정", href: "#/action-2" },
+    { label: "개념 추가", href: "#/action-2" },
+  ];
 
   return (
     <div
@@ -43,14 +53,24 @@ const DetailView = () => {
             alt="북마크"
           />
         </div>
+        <div>
+          <DropdwonButton
+            title="신고"
+            variant="danger"
+            items={reportDropdown}
+            onItemClick=""
+          />
+          <DropdwonButton
+            title="수정"
+            variant="success"
+            items={updateDropdown}
+            onItemClick=""
+          />
+        </div>
       </div>
 
       <div className="imgContainer">
-        <Image
-          style={{ marginBottom: "15px" }}
-          roundedCircle
-          src={content.img}
-        />
+        <Image style={{ marginBottom: "15px" }} fluid src={content.img} />
       </div>
 
       <div
