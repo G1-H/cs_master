ad7468ab79d2070fca6900b7722e1f1030dd1b8e
G1-H <hwangjiwon1992@gmail.com>
2025-02-11 23:21:47 +0900

    Feat : MemberRepository 메서드 추가
    findByProviderIdAndProvider(

--- src/main/java/com/csmaster/cs_master/repository/MemberRepository.java
+++ src/main/java/com/csmaster/cs_master/repository/MemberRepository.java
@@ -2,6 +2,8 @@ package com.csmaster.cs_master.repository;
 
 import com.csmaster.cs_master.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,8 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByEmail(String email);
 
     Optional<Member> findByNickname(String nickname);
+
+    @Query("SELECT m FROM Member m WHERE m.providerId = :providerId AND m.provider = :provider")
+    Optional<Member> findByProviderIdAndProvider(@Param("providerId") String providerId, @Param("provider") String provider);
+
 }
