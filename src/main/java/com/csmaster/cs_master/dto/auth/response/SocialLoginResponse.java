16ba336927a733de43e69cb644075d7384d64889
G1-H <hwangjiwon1992@gmail.com>
2025-02-14 12:53:26 +0900

    Feat : 회원가입 시 비밀번호 암호화 작업

--- src/main/java/com/csmaster/cs_master/dto/auth/response/SocialLoginResponse.java
+++ src/main/java/com/csmaster/cs_master/dto/auth/response/SocialLoginResponse.java
@@ -1,19 +1,14 @@
 package com.csmaster.cs_master.dto.auth.response;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.Setter;
 
-@Setter
 @Getter
+@AllArgsConstructor
+@Deprecated
 public class SocialLoginResponse {
 
     private String providerId;
     private String email;
     private Boolean isRegistered;
-
-    public SocialLoginResponse(String providerId, String email, Boolean isRegistered) {
-        this.providerId = providerId;
-        this.email = email;
-        this.isRegistered = isRegistered;
-    }
 }
