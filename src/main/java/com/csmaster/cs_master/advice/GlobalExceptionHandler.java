d69ab05f916f0b006c4b359ffb009844912cc1d6
G1-H <hwangjiwon1992@gmail.com>
2025-02-14 10:42:59 +0900

    Feat : 소셜 로그인 이셉션 핸들러 (Deprecated)
    해당 이셉션 핸들러 필요하다고 만들었으나 현재 필요 X

--- src/main/java/com/csmaster/cs_master/advice/GlobalExceptionHandler.java
+++ src/main/java/com/csmaster/cs_master/advice/GlobalExceptionHandler.java
@@ -1,6 +1,8 @@
 package com.csmaster.cs_master.advice;
 
+import com.csmaster.cs_master.dto.auth.response.SocialLoginResponse;
 import com.csmaster.cs_master.exception.CustomException;
+import com.csmaster.cs_master.exception.domain.SocialLoginException;
 import com.csmaster.cs_master.utils.ApiUtil;
 import com.csmaster.cs_master.vo.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -8,7 +10,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.HashMap;
@@ -23,7 +24,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Response<Map<String, String>>> handleValidationExceptions(MethodArgumentNotValidException e) {
         Map<String, String> errors = new HashMap<>();
         e.getBindingResult().getFieldErrors().forEach(error ->
-            errors.put(error.getField(), error.getDefaultMessage())
+                errors.put(error.getField(), error.getDefaultMessage())
         );
         log.error("\u001B[31mcode: " + HttpStatus.BAD_REQUEST.value() + "\u001B[0m");
         log.error("\u001B[31mmessage: " + e.getMessage() + "\u001B[0m");
@@ -31,21 +32,27 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(CustomException.class)
-    public  ResponseEntity<Response<Object>> handleCustomException(CustomException e) {
-        log.error("\u001B[31mcode: "+e.getErrorStatus().value()+"\u001B[0m");
-        log.error("\u001B[31mmessage: "+ e.getErrorMessage()+"\u001B[0m");
+    public ResponseEntity<Response<Object>> handleCustomException(CustomException e) {
+        log.error("\u001B[31mcode: " + e.getErrorStatus().value() + "\u001B[0m");
+        log.error("\u001B[31mmessage: " + e.getErrorMessage() + "\u001B[0m");
         return ApiUtil.fail(e.getErrorStatus(), e.getErrorMessage(), null);
     }
 
+    //소셜 로그인 시 추가 정보 필요
+    @Deprecated
+    @ExceptionHandler(SocialLoginException.class)
+    public ResponseEntity<Response<SocialLoginResponse>> handleSocialLoginException(SocialLoginException e) {
+        log.info(" " + e.getErrorStatus().value());
+        log.info(" " + e.getErrorMessage());
+        return ApiUtil.fail(e.getErrorStatus(), e.getErrorMessage(), e.getSocialLoginResponse());
+    }
 
-//
-//    @ExceptionHandler(RuntimeException.class)
-//    public ResponseEntity<Response<Object>> handleRuntimeException(RuntimeException e) {
-//        log.error("\u001B[31mcode: "+ HttpStatus.INTERNAL_SERVER_ERROR.value() +"\u001B[0m");
-//        log.error("\u001B[31mmessage: "+ e.getMessage()+"\u001B[0m");
-//        return ApiUtil.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), null);
-//    }
-
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<Response<Object>> handleRuntimeException(RuntimeException e) {
+        log.error("\u001B[31mcode: "+ HttpStatus.INTERNAL_SERVER_ERROR.value() +"\u001B[0m");
+        log.error("\u001B[31mmessage: "+ e.getMessage()+"\u001B[0m");
+        return ApiUtil.fail(HttpStatus.INTERNAL_SERVER_ERROR, e.getMessage(), null);
+    }
 
 
 }
