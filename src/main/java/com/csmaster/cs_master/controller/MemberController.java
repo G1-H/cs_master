69c4451899f1910fcf60d6edef10d1a645fa6964
G1-H <hwangjiwon1992@gmail.com>
2025-02-14 11:09:12 +0900

    Refactor : 엔드포인트 변경 (users -> members)

--- src/main/java/com/csmaster/cs_master/controller/MemberController.java
+++ src/main/java/com/csmaster/cs_master/controller/MemberController.java
@@ -1,11 +1,10 @@
 package com.csmaster.cs_master.controller;
 
-import com.csmaster.cs_master.dto.member.SignUpRequest;
+import com.csmaster.cs_master.dto.member.request.SignUpRequest;
 import com.csmaster.cs_master.service.MemberService;
 import com.csmaster.cs_master.utils.ApiUtil;
 import com.csmaster.cs_master.vo.Response;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,29 +12,29 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/users")
+@RequestMapping("/api/members")
 public class MemberController {
 
     private final MemberService memberService;
 
     @PostMapping("/sign-up")
-    public ResponseEntity<Response<Void>> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
-            String email =  memberService.signUp(signUpRequest);
-        return ApiUtil.success(HttpStatus.CREATED, email + " 가입 완료", null);
+    public ResponseEntity<Response<Object>> signUp(@Valid @RequestBody SignUpRequest signUpRequest) {
+        Object response  = memberService.signUp(signUpRequest);
+        return ApiUtil.success(HttpStatus.CREATED, "회원가입 완료", response);
     }
 
     //이메일 중복 확인
-    @GetMapping("/check-email-duplication")
+    @GetMapping("/sign-up/check-email-duplication")
     public ResponseEntity<Response<Void>> checkEmail( @RequestParam(value = "email") String email) {
         memberService.checkEmail(email);
-        return ApiUtil.success(HttpStatus.OK, email + "이메일 중복되지 않습니다.", null);
+        return ApiUtil.success(HttpStatus.OK, email + " 이메일 중복되지 않습니다.", null);
     }
 
     //넥네임 중복 확인
-    @GetMapping("/check-nickname-duplication")
+    @GetMapping("/sign-up/check-nickname-duplication")
     public ResponseEntity<Response<Void>> checkNickname(@RequestParam(value = "nickname") String nickname) {
         memberService.checkNickname(nickname);
-        return ApiUtil.success(HttpStatus.OK, nickname + "닉네임 중복되지 않습니다.", null);
+        return ApiUtil.success(HttpStatus.OK, nickname + " 닉네임 중복되지 않습니다.", null);
     }
 
 }
